0f202c3af3379d11b632d2e07703de8ae9ec9be6
efthymisgeo <efthymis.g.georgiou@gmail.com>
2020-03-24 23:23:30 +0200

    modified viztool

--- utils/visualize_curves.py
+++ utils/visualize_curves.py
@@ -2,6 +2,7 @@ import os
 import sys
 import copy
 import json
+import argparse
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -21,14 +22,38 @@ REF_DICT_NAMES = {
 }
 
 
+def parse_args():
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-m", "--mdl_config", required=True,
+    #                     help="Path to model configuration file"),
+    # parser.add_argument("-e", "--exp_config", required=False,
+    #                     default="configs/exp_config.json",
+    #                     help="Path to experiment configuration file."
+    #                          "Handles optimizer, lr, batch size, etc")
+    # parser.add_argument("-d", "--dt_config", required=False,
+    #                     default="configs/mnist_config.json",
+    #                     help="Path to data cnfiguration fil
+    # TODO
+    pass
+
+
 def plot_dict(ax, t, x, id, line, title="loss curves"):
-    """plots a list of values
+    """
+    Plots a list of values
+    Args:
+        ax:
+        t (list): list of ints which relate to the x-axis
+        x (dict): {"train":[], "val":[], "test":[]}
+        id (str): plot identifier string e.g smart-drop
+        line (str): line identifier
+        title (str):
+        skip_val (bool): flag to skip validation set values
     """
     #fig, ax = plt.subplots()
-    plt.xticks(t, t)
+    #plt.xticks(t, t, rotation=30)
     #ax.set_title(title)
     for k, v in x.items():
-        print(len(v))
+        if k == "val" and skip_val: continue
         ax.plot(t, v, line, label=id+"_"+k)
     ax.legend()
 
@@ -36,18 +61,37 @@ def plot_dict(ax, t, x, id, line, title="loss curves"):
 if __name__ == "__main__":
     #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
     #plt.rc('text', usetex=True)
+    accuracies = True
+    if accuracies:
+        figname = "Accuracy (%)"
+        save_as = "acc.png"
+    else:
+        figname = "Loss"
+        save_as = "loss.png"
+   
     path1 = "experiments/CIFAR10/_plain_cifar_0.01/CNN2D__plain_cifar_0.01_run_0.json"
     path2 = "experiments/CIFAR10/_dropout_0.5_0.01/CNN2D__dropout_0.5_0.01_run_0.json"
-    path3 = "experiments/CIFAR10/_plain_cifar_0.01/CNN2D__plain_cifar_0.01_run_0.json"
-    paths = [(path1, "no-drop", "-") , (path2, "drop", "--")]
+    path3 = "experiments/CIFAR10/_smart_0.001_0.00004/CNN2D__smart_0.001_0.00004_run_0.json"
+
     
-    path1 = "experiments/CIFAR10/_plain_cifar_0.01/CNN2D__plain_cifar_0.01_run_0.json"
-    path2 = "experiments/CIFAR10/_dropout_0.5_0.01/CNN2D__dropout_0.5_0.01_run_0.json"
-    path3 = "experiments/CIFAR10/_plain_cifar_0.01/CNN2D__plain_cifar_0.01_run_0.json"
-    paths = [(path1, "no-drop", "-") , (path2, "drop", "--")]
+    # paths = [(path1, "no-drop", "-") ,
+    #          (path2, "drop", "--"),
+    #          (path3, "drop", "-*")]
+    
+    skip_val = True
+
+    # path1 = "experiments/CIFAR10/_plain_cifar_0.01/CNN2D__plain_cifar_0.01_run_0.json"
+    # path2 = "experiments/CIFAR10/_dropout_0.5_0.01/CNN2D__dropout_0.5_0.01_run_0.json"
+    # path3 = "experiments/CIFAR10/_smart_0.001_0.00004/CNN2D__smart_0.001_0.00004_run_0.json"
+    paths = [(path1, "no-drop", "-"),
+             (path2, "drop", "--"),
+             (path3, "smart-drop", "-.")]
     mpl.style.use("ggplot")
 
     fig, ax = plt.subplots()
+    plt.yscale('linear')
+    plt.xscale('linear')
+    connectionstyle='angle, angleA=-120, angleB=180, rad=10'
     for pth, nm, line in paths:
         with open (pth, "r") as fd:
             summary = json.load(fd)
@@ -58,9 +102,24 @@ if __name__ == "__main__":
         test_acc = summary["test_acc"]
         # hack to work
         # should be removed in the future
-        losses["test"].append(losses["test"][-1])
-        t = np.arange(0, len(losses["train"]))
-        plot_dict(ax, t, losses, nm, line)
+        if nm != "smart-drop":
+            accuracies["test"].append(accuracies["test"][-1])
+            losses["test"].append(losses["test"][-1])
+        #t = np.arange(0, len(losses["train"]))
+        if accuracies:
+            t = np.arange(0, len(accuracies["train"]))
+            ax.annotate(str(test_acc),
+                        xy=(best_epoch, test_acc),
+                        xytext=(best_epoch - 4, 15 + test_acc),
+                        arrowprops=dict(arrowstyle="->",
+                                        connectionstyle=connectionstyle,
+                                        facecolor='white',
+                                        shrink=0.05))
+            plot_dict(ax, t, accuracies, nm, line, skip_val)
+        else:
+            t = np.arange(0, len(losses["train"]))    
+            plot_dict(ax, t, losses, nm, line, skip_val)
     plt.xlabel('Epochs')
-    plt.ylabel('Loss')    
-    plt.savefig('losses.png', bbox_inches='tight')
+    plt.ylabel(figname)
+    plt.savefig(save_as, bbox_inches='tight')
+    
