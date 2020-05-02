a8159b638600b9ac2d3a1e87bc166132800cafe2
efthymisgeo <efthymis.g.georgiou@gmail.com>
2020-05-02 20:30:34 +0300

    added cluster plain drop conf files

--- aris/job_drop2.sh
+++ aris/job_drop2.sh
@@ -7,15 +7,15 @@
 #                                  #
 ####################################
 
-#SBATCH --job-name=raw-small    # DO NOT FORGET TO CHANGE THIS
-#SBATCH --output=logs/raw-small.%j.out # DO NOT FORGET TO CHANGE THIS. the job stdout will be dumped here. (%j expands to jobId).
-#SBATCH --error=logs/raw-small.%j.err # DO NOT FORGET TO CHANGE THIS. the job stdout will be dumped here. (%j expands to jobId).
+#SBATCH --job-name=drop2-small    # DO NOT FORGET TO CHANGE THIS
+#SBATCH --output=logs/drop2-small.%j.out # DO NOT FORGET TO CHANGE THIS. the job stdout will be dumped here. (%j expands to jobId).
+#SBATCH --error=logs/drop2-small.%j.err # DO NOT FORGET TO CHANGE THIS. the job stdout will be dumped here. (%j expands to jobId).
 #SBATCH --ntasks=1     # How many times the command will run. Leave this to 1 unless you know what you are doing
 #SBATCH --nodes=1     # The task will break in so many nodes. Use this if you need many GPUs
 #SBATCH --gres=gpu:1 # GPUs per node to be allocated
 #SBATCH --ntasks-per-node=1     # Same as ntasks
 #SBATCH --cpus-per-task=4     # If you need multithreading
-#SBATCH --time=24:00:00   # HH:MM:SS Estimated time the job will take. It will be killed if it exceeds the time limit
+#SBATCH --time=48:00:00   # HH:MM:SS Estimated time the job will take. It will be killed if it exceeds the time limit
 #SBATCH --mem=8G   # memory to be allocated per NODE
 #SBATCH --partition=gpu    # gpu: Job will run on one or more of the nodes in gpu partition. ml: job will run on the ml node
 #SBATCH --account=pa181004    # DO NOT CHANGE THIS
@@ -42,6 +42,10 @@ module load python/3.6.5
 module load pytorch/1.3.1
 module load slp/1.3.1
 
+
 ## RUN YOUR PROGRAM ##
 cd .
-srun python models/regbi.py -m configs/model/layers-80sec_shallow.json -d configs/dataset/cifar10.json -e configs/experiment/aris/plain_cifar10.json
+srun python models/regbi.py \
+-m configs/model/layers-80sec_shallow.json \
+-d configs/dataset/cifar10.json \
+-e configs/experiment/aris/drop2_cifar10.json
